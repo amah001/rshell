3deff27260349ea053a451e00f36d86b4ecb0590
Alex <amah001@ucr.edu>
2015-04-13 00:37:00 -0700

    parses the string before the first connector

--- test.cpp
+++ test.cpp
@@ -10,6 +10,7 @@
 #include <string.h>
 //#include <boost/tokenizer.hpp>
 using namespace std;
+//using namespace boost;
 void displayPrompt()
 {
 	if(getlogin() == NULL)
@@ -25,9 +26,13 @@ void displayPrompt()
 	return;
 }
 /*
-void commandList(string input, vector<vector<strings> commands)
+void commandList(*char input, vector<vector<string> > commands)
 {
-	
+	while(input != NULL)
+	{
+		commands.push_back(input);
+	}
+	return;
 }
 */
 int main(int argc, char**argv)
@@ -35,33 +40,54 @@ int main(int argc, char**argv)
 	//connectors
 	string executeNext = ";";	//execute next command no matter what
 	string ifWorkExecute = "&&";	//execute next command if previous command succeeds
-	string ifFailExecute = "||";	//execute next command if previous command fails 
-displayPrompt();
-cout << " hi kenny" << endl;
-/*
+	string ifFailExecute = "||";	//execute next command if previous command fails
+
+//	char_separator<char> semicolon(executeNext);
+//	char_separator<char> Ands(ifWorkExecute);
+//	char_separator<char> Ors(ifFailExecute);
 	while(1)
 	{
 		displayPrompt();
-		string command_input;
-		vector<vector<strings> commands_calls;
-		getline(cin, command_input);
+		string  command_input;
+		vector<char*> commands_calls;
+		getline(cin,command_input);	//takes in input
+		char* first_token;
+		char command_char[command_input.size()];
+		strcpy(command_char,command_input.c_str());
+//	commandList(command_input,command_calls);
+		cout << command_input << endl;
+		//char* test = (char*)command_char;
+		char* temp_token;
+		size_t s_find = command_input.find(executeNext);
+		size_t a_find = command_input.find(ifWorkExecute);
+		size_t o_find = command_input.find(ifFailExecute);
+		if(s_find < a_find && s_find < o_find)
+		{
+			first_token = strtok(command_char,";");
+			commands_calls.push_back(first_token);
+		}
+		else if(a_find < s_find && a_find < o_find)
+		{
+			first_token = strtok(command_char,"&&");	
+			commands_calls.push_back(first_token);
+		}
+		else if(o_find < a_find && o_find < s_find)
+		{
+			first_token = strtok(command_char,"||");	
+			commands_calls.push_back(first_token);
+		}
+		cout << commands_calls[0] << endl;
+/*
+for(int i = 0; i < command_input.size(); i++)
+		{
+			//size_t s_find = command_input.find(executeNext);
+			//size_t a_find = command_input.find(ifWorkExecute);
+			//size_t o_find = command_input.find(ifFailExecute);
+		}
+*/		break;
+}
 
-		break;
-	}
 	
-*/
-
-
-
-
-
-
-
-
-
-
-
-
 
 return 0;
 }
