20845ede78608d5abd47ac63a6624937748de92e
Alex <amah001@ucr.edu>
2015-04-16 05:09:12 -0700

    scrapped trash code, fixed some parse

--- test.cpp
+++ test.cpp
@@ -33,18 +33,13 @@ void commandList(*char input, vector<vector<string> > commands)
 		commands.push_back(input);
 	}
 	return;
-}
-*/
-int main(int argc, char**argv)
-{
-	//connectors
+} */ int main(int argc, char**argv) { 
+	//connectors 
 	string executeNext = ";";	//execute next command no matter what
 	string ifWorkExecute = "&&";	//execute next command if previous command succeeds
 	string ifFailExecute = "||";	//execute next command if previous command fails
 
-//	char_separator<char> semicolon(executeNext);
-//	char_separator<char> Ands(ifWorkExecute);
-//	char_separator<char> Ors(ifFailExecute);
+
 	while(1)
 	{
 		displayPrompt();
@@ -52,41 +47,32 @@ int main(int argc, char**argv)
 		vector<char*> commands_calls;
 		getline(cin,command_input);	//takes in input
 		char* first_token;
-		char command_char[command_input.size()];
-		strcpy(command_char,command_input.c_str());
-//	commandList(command_input,command_calls);
+		char* command_char = (char*)command_input.c_str();
+		//strcpy(command_charz,command_input.c_str());
 		cout << command_input << endl;
+		cout << command_char << endl;
+		command_char = strtok(command_char," ");	
 		//char* test = (char*)command_char;
 		char* temp_token;
 		size_t s_find = command_input.find(executeNext);
 		size_t a_find = command_input.find(ifWorkExecute);
 		size_t o_find = command_input.find(ifFailExecute);
-		if(s_find < a_find && s_find < o_find)
+		while(command_char != NULL)
 		{
-			first_token = strtok(command_char,";");
-			commands_calls.push_back(first_token);
+			cout << command_char << endl;
+			command_char = strtok(NULL," ");
 		}
-		else if(a_find < s_find && a_find < o_find)
-		{
-			first_token = strtok(command_char,"&&");	
-			commands_calls.push_back(first_token);
-		}
-		else if(o_find < a_find && o_find < s_find)
-		{
-			first_token = strtok(command_char,"||");	
-			commands_calls.push_back(first_token);
-		}
-		cout << commands_calls[0] << endl;
+/*
 		int pid = fork();
 		if (pid == -1)
 		{
 			perror("fork");
-			return 0;
+			_exit(1);
 		}
 		else if(pid == 0)
 		{
 			cout << "This is the child process" << endl;
-			return 0;
+			_exit(1);
 		}
 		else if(pid > 0)
 		{
@@ -95,18 +81,8 @@ int main(int argc, char**argv)
 				perror("wait(0)");
 			}
 		}
-
-
-
-
-/*
-for(int i = 0; i < command_input.size(); i++)
-		{
-			//size_t s_find = command_input.find(executeNext);
-			//size_t a_find = command_input.find(ifWorkExecute);
-			//size_t o_find = command_input.find(ifFailExecute);
-		}
-*/		break;
+*/
+		//break;
 }
 
 	
