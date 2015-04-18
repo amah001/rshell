7491131dfbaaa4fab177a098f3f1885b6a928a54
Alex Mah <amah001@hammer.cs.ucr.edu>
2015-04-18 03:29:53 -0700

    parses connectors

--- src/test.cpp
+++ src/test.cpp
@@ -42,9 +42,10 @@ bool exitCheck(char* exitCommand)
 }
  int main(int argc, char**argv) { 
 	//connectors 
-	string executeNext = ";";	//execute next command no matter what
-	string ifWorkExecute = "&&";	//execute next command if previous command succeeds
-	string ifFailExecute = "||";	//execute next command if previous command fails
+    string spaces = " ";
+	string RunNext = ";";	    //run next command no matter what
+	string ifWorkRun = "&&";	//run next command if previous command works
+	string ifFailRun = "||";	//run next command if previous command fails
 
 
 	while(1)
@@ -53,7 +54,6 @@ bool exitCheck(char* exitCommand)
 		string  command_input;
 		vector<char*> commands_calls;
 		getline(cin,command_input);	//takes in input
-		
         int commentLocater = command_input.find("#");
 		if(commentLocater != -1)
 		{
@@ -61,35 +61,75 @@ bool exitCheck(char* exitCommand)
 		}
         cout << command_input << endl;
         string::iterator my_iterator;
-        int position;
-        /*
-        for( my_iterator = command_input.begin(),my_iterator < command_input.end(),my_iterator++)
+        int position = 0;
+        
+        for( my_iterator = command_input.begin();
+        my_iterator < command_input.end();
+        my_iterator++, position++)
         {
-            int NextFind = command_input.find(executeNext);     //looks for ;
-            int ifWorkFind = command_input.find(ifWorkExecute);   //looks for &&
-            int ifFailFind = command_input.find(ifFailExecute);   //looks for ||
-            if(NextFind < ifWorkFind && NextFind < ifFailFind & NextFind != -1)
+            // puts spaces around connectors
+            int NextFind = command_input.find(RunNext,position);
+                //looks for ;
+            int ifWorkFind = command_input.find(ifWorkRun,position);
+                //looks for &&
+            int ifFailFind = command_input.find(ifFailRun,position);
+                //looks for ||
+            if(((NextFind < ifWorkFind && NextFind < ifFailFind) 
+                || (ifWorkFind == -1 && ifFailFind ==-1)
+                || (NextFind < ifWorkFind && ifFailFind ==-1)
+                || (NextFind < ifFailFind && ifWorkFind ==-1))
+                && (NextFind != -1))
             {
+                
                 //adds spaces around ; connector
-                command_input.insert(NextFind, " ");
-                command_input.insert(command_input.find(executeNext)+1," ")
-                my_iterator = command_input.find(executeNext)+1;
+                command_input.insert(NextFind, spaces);
+                command_input.insert(command_input.find(RunNext,position)+1," ");
+                my_iterator = command_input.begin() + command_input.find(RunNext)+1;
+                position = command_input.find(RunNext, position)+1;
             }
+            else if(((ifWorkFind < NextFind && ifWorkFind < ifFailFind) 
+                || (NextFind == -1 && ifFailFind ==-1)
+                || (ifWorkFind < NextFind && ifFailFind ==-1)
+                || (ifWorkFind < ifFailFind && NextFind ==-1))
+                && (ifWorkFind != -1))
+            {
+                
+                //adds spaces around && connector
+                command_input.insert(ifWorkFind, spaces);
+                command_input.insert(command_input.find(ifWorkRun,position)+2," ");
+                my_iterator = command_input.begin() + command_input.find(ifWorkRun)+2;
+                position = command_input.find(ifWorkRun, position)+2;
+            }
+            else if(((ifFailFind < ifWorkFind && ifFailFind < NextFind) 
+                || (ifWorkFind == -1 && NextFind ==-1)
+                || (ifFailFind < ifWorkFind && NextFind ==-1)
+                || (ifFailFind < NextFind && ifWorkFind ==-1))
+                && (ifFailFind != -1))
+            {
+                
+                //adds spaces around || connector
+                command_input.insert(ifFailFind, spaces);
+                command_input.insert(command_input.find(ifFailRun,position)+2," ");
+                my_iterator = command_input.begin() + command_input.find(ifFailRun)+2;
+                position = command_input.find(ifFailRun, position)+2;
+            }
+            //cout << command_input << endl;
         }
-        */
+        
 		char* command_char = new char[command_input.size()];
 		strcpy(command_char,command_input.c_str());
 		char *iterator_token = strtok(command_char," ");
 		while(iterator_token !=0)
 		{
-			cout << iterator_token << endl;
-			iterator_token = strtok(NULL," ");
 			/*
             if(exitCheck(iterator_token));
 			{
 				exit(1);
 			}
             */
+			cout << iterator_token << endl;
+            iterator_token = strtok(NULL," ");
+            
 		}
 	}
 	return 0;
