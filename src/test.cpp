e7f100abc99d6e1547908f3f1e07f0feb3a70a3f
Alex Mah <amah001@ucr.edu>
2015-04-18 19:14:14 -0700

    made parse into a function

--- src/test.cpp
+++ src/test.cpp
@@ -40,6 +40,70 @@ bool exitCheck(char* exitCommand)
 		return false;
 	}
 }
+void parser(string &string_input)
+{
+    int commentLocater = string_input.find("#");
+		if(commentLocater != -1)
+		{
+			string_input = string_input.substr(0,commentLocater);
+		}
+        //cout << string_input << endl;
+        string::iterator my_iterator;
+        int position = 0;
+        
+        for( my_iterator = string_input.begin();
+        my_iterator < string_input.end();
+        my_iterator++, position++)
+        {
+            // puts spaces around connectors
+            int NextFind = string_input.find(";",position);
+                //looks for ;
+            int ifWorkFind = string_input.find("&&",position);
+                //looks for &&
+            int ifFailFind = string_input.find("||",position);
+                //looks for ||
+            if(((NextFind < ifWorkFind && NextFind < ifFailFind) 
+                || (ifWorkFind == -1 && ifFailFind ==-1)
+                || (NextFind < ifWorkFind && ifFailFind ==-1)
+                || (NextFind < ifFailFind && ifWorkFind ==-1))
+                && (NextFind != -1))
+            {
+                
+                //adds spaces around ; connector
+                string_input.insert(NextFind, " ");
+                string_input.insert(string_input.find(";",position)+1," ");
+                my_iterator = string_input.begin() + string_input.find(";")+1;
+                position = string_input.find(";", position)+1;
+            }
+            else if(((ifWorkFind < NextFind && ifWorkFind < ifFailFind) 
+                || (NextFind == -1 && ifFailFind ==-1)
+                || (ifWorkFind < NextFind && ifFailFind ==-1)
+                || (ifWorkFind < ifFailFind && NextFind ==-1))
+                && (ifWorkFind != -1))
+            {
+                
+                //adds spaces around && connector
+                string_input.insert(ifWorkFind, " ");
+                string_input.insert(string_input.find("&&",position)+2," ");
+                my_iterator = string_input.begin() + string_input.find("&&")+2;
+                position = string_input.find("&&", position)+2;
+            }
+            else if(((ifFailFind < ifWorkFind && ifFailFind < NextFind) 
+                || (ifWorkFind == -1 && NextFind ==-1)
+                || (ifFailFind < ifWorkFind && NextFind ==-1)
+                || (ifFailFind < NextFind && ifWorkFind ==-1))
+                && (ifFailFind != -1))
+            {
+                
+                //adds spaces around || connector
+                string_input.insert(ifFailFind, " ");
+                string_input.insert(string_input.find("||",position)+2," ");
+                my_iterator = string_input.begin() + string_input.find("||")+2;
+                position = string_input.find("||", position)+2;
+            }
+            //cout << string_input << endl;
+        }
+}
  int main(int argc, char**argv) { 
 	//connectors 
     string spaces = " ";
@@ -54,68 +118,8 @@ bool exitCheck(char* exitCommand)
 		string  command_input;
 		vector<char*> commands_calls;
 		getline(cin,command_input);	//takes in input
-        int commentLocater = command_input.find("#");
-		if(commentLocater != -1)
-		{
-			command_input = command_input.substr(0,commentLocater);
-		}
-        cout << command_input << endl;
-        string::iterator my_iterator;
-        int position = 0;
-        
-        for( my_iterator = command_input.begin();
-        my_iterator < command_input.end();
-        my_iterator++, position++)
-        {
-            // puts spaces around connectors
-            int NextFind = command_input.find(RunNext,position);
-                //looks for ;
-            int ifWorkFind = command_input.find(ifWorkRun,position);
-                //looks for &&
-            int ifFailFind = command_input.find(ifFailRun,position);
-                //looks for ||
-            if(((NextFind < ifWorkFind && NextFind < ifFailFind) 
-                || (ifWorkFind == -1 && ifFailFind ==-1)
-                || (NextFind < ifWorkFind && ifFailFind ==-1)
-                || (NextFind < ifFailFind && ifWorkFind ==-1))
-                && (NextFind != -1))
-            {
-                
-                //adds spaces around ; connector
-                command_input.insert(NextFind, spaces);
-                command_input.insert(command_input.find(RunNext,position)+1," ");
-                my_iterator = command_input.begin() + command_input.find(RunNext)+1;
-                position = command_input.find(RunNext, position)+1;
-            }
-            else if(((ifWorkFind < NextFind && ifWorkFind < ifFailFind) 
-                || (NextFind == -1 && ifFailFind ==-1)
-                || (ifWorkFind < NextFind && ifFailFind ==-1)
-                || (ifWorkFind < ifFailFind && NextFind ==-1))
-                && (ifWorkFind != -1))
-            {
-                
-                //adds spaces around && connector
-                command_input.insert(ifWorkFind, spaces);
-                command_input.insert(command_input.find(ifWorkRun,position)+2," ");
-                my_iterator = command_input.begin() + command_input.find(ifWorkRun)+2;
-                position = command_input.find(ifWorkRun, position)+2;
-            }
-            else if(((ifFailFind < ifWorkFind && ifFailFind < NextFind) 
-                || (ifWorkFind == -1 && NextFind ==-1)
-                || (ifFailFind < ifWorkFind && NextFind ==-1)
-                || (ifFailFind < NextFind && ifWorkFind ==-1))
-                && (ifFailFind != -1))
-            {
-                
-                //adds spaces around || connector
-                command_input.insert(ifFailFind, spaces);
-                command_input.insert(command_input.find(ifFailRun,position)+2," ");
-                my_iterator = command_input.begin() + command_input.find(ifFailRun)+2;
-                position = command_input.find(ifFailRun, position)+2;
-            }
-            //cout << command_input << endl;
-        }
-        
+        parser(command_input);  //parses stuff
+
 		char* command_char = new char[command_input.size()];
 		strcpy(command_char,command_input.c_str());
 		char *iterator_token = strtok(command_char," ");
