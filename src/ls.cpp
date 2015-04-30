9e1a02a1d0019387db9913f0ca9f1557985d5c0b
Alex <amah001@ucr.edu>
2015-04-30 03:36:39 -0700

    created framework for -l

--- src/ls.cpp
+++ src/ls.cpp
@@ -13,29 +13,34 @@
 #include <dirent.h>
 #include <errno.h>
 #include <algorithm>
-
+#include <pwd.h>
+#include <grp.h>
+#include <sys/stat.h>
 using namespace std;
 
-void directoryRunthrough(vector<string> files, char* argv[], string function)
+void directoryRunthrough(vector<string>& files, char* argv[], string function)
 {
 	DIR *dirp;
-	if(function == "ls")
-	{	
+	//if(function == "ls")
+	//{
+	//	cout << "run"<< endl;
 		if((dirp = opendir(".")) == NULL)
 		{
 			perror("opendir()");
-			exit(1);
+			_exit(1);
 		}
 		
-	}
+	//}
+	/*
 	else
 	{
 		if((dirp = opendir(argv[1])) == NULL)
 		{
 			perror("opendir()");
-			exit(1);
+			_exit(1);
 		}
 	}
+	*/
 	struct dirent *filespecs;
 	errno = 0;
 	while ((filespecs = readdir(dirp)) != NULL)
@@ -45,12 +50,12 @@ void directoryRunthrough(vector<string> files, char* argv[], string function)
 	if(errno != 0)
 	{
 		perror("readdir()");
-		exit(1);
+		_exit(1);
 	}
 	if(closedir(dirp) == -1)
 	{
 		perror("closedir()");
-		exit(1);
+		_exit(1);
 	}
 	sort(files.begin(),files.end());
 
@@ -61,22 +66,118 @@ void print(char*argv[])
 	vector<string> files;
 	string directoryz = "ls";
 	directoryRunthrough(files,argv,directoryz);
-	for(int i = 0; i < files.size();i++)
+	for(unsigned int i = 0; i < files.size();i++)
 	{
-	//	if(files[i][0] == '.')
-	//	{
+		if(files[i][0] != '.')
+		{
 			cout << files[i] << endl;
-	//	}
+		}
 	}
 	return;
 }
 void printAll(char* argv[])
 {
+	vector<string> files;
+	string directoryz = "a";
+	directoryRunthrough(files,argv,directoryz);
 	return;
 }
-void printLong()
+void printLong(char* argv[])
 {
+	vector<string> files;
+	string directoryz = "l";
+	cout << "h" << endl;
+	directoryRunthrough(files,argv,directoryz);
+	struct stat info;
+	struct passwd *pwd;
+	struct group *grp;
+	string owner;
+	string group;
+	int links;
+	cout << "1h" << endl;
+	string dot = ".";
+	if(stat(dot.c_str(),&info) == -1)
+	{
+		perror("stat");
+		_exit(1);
+	}
+	cout << "2h" << endl;
+	if((pwd = getpwuid(info.st_uid)) == NULL)
+	{
+		perror("getpwuid()");
+		_exit(1);
+	}
+	cout << "3h" << endl;
+	owner = pwd->pw_name;
+	if((grp = getgrgid(info.st_gid)) == NULL)
+	{
+		perror("getgrgid()");
+		_exit(1);
+	}
+	cout << "4h" << endl;
+	group = grp->gr_name;
+	if(S_ISREG(info.st_mode))
+		cout << "-";
+	else if(S_ISDIR(info.st_mode))
+		cout << "d";
+	else if(S_ISCHR(info.st_mode))
+		cout << "c";
+	else if(S_ISBLK(info.st_mode))
+		cout << "b";
+	else if(S_ISFIFO(info.st_mode))
+		cout << "p";
+	else if(S_ISLNK(info.st_mode))
+		cout << "l";
+	else if(S_ISSOCK(info.st_mode))
+		cout << "s";
 
+	if(S_IRUSR & info.st_mode)
+		cout << "r";
+	else
+		cout << "-";
+	if(S_IWUSR & info.st_mode)
+		cout << "w";
+	else
+		cout << "-";
+	if(S_IXUSR & info.st_mode)
+		cout << "x";
+	else
+		cout << "-";
+
+	if(S_IRGRP & info.st_mode)
+		cout << "r";
+	else
+		cout << "-";
+	if(S_IWGRP & info.st_mode)
+		cout << "w";
+	else
+		cout << "-";
+	if(S_IXGRP & info.st_mode)
+		cout << "x";
+	else
+		cout << "-";
+
+	if(S_IROTH & info.st_mode)
+		cout << "r";
+	else
+		cout << "-";
+	if(S_IWOTH & info.st_mode)
+		cout << "w";
+	else
+		cout << "-";
+	if(S_IXOTH & info.st_mode)
+		cout << "x ";
+	else
+		cout << "- ";
+	//*************************************************
+	// do some alignment thingy
+	cout << info.st_nlink << " ";	//prints # of links
+	cout << pwd->pw_name << " ";	//prints user
+	cout << grp->gr_name << " ";	//prints group
+	cout << info.st_blksize << " "; //prints size
+	cout << ctime(&info.st_mtime) << " ";
+	cout << files[0] << endl;
+	return;
 }
 void printRecursive()
 {
@@ -86,11 +187,11 @@ void printRecursive()
 int main(int argc, char* argv[])
 {
 	vector<string> lists;
-	if(argc == 2)
+	if(argc == 1)
 	{
 		print(argv);
 	}
-
+	printLong(argv);
 	/*
 	for(int i = 0; i < lists.size(); i++)
 	{
