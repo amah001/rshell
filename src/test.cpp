4af74e5943e9a8564b3423ddece05e1bef19d345
Alex <amah001@ucr.edu>
2015-05-13 03:59:51 -0700

    < and >> are parsed, changed if statements

--- src/test.cpp
+++ src/test.cpp
@@ -31,7 +31,7 @@ void displayPrompt()
 		_exit(1);
 	}
     
-	cout << login << "@" << host  << ":~$";
+	cout << login << "@" << host  << ":$";
 	return;
 }
 
@@ -66,7 +66,7 @@ void separator_parser(string &string_input)
     //cout << string_input << endl;
     string::iterator my_iterator;
     int position = 0;
-    
+    int counter = 0;
     for( my_iterator = string_input.begin();
     my_iterator < string_input.end();
     my_iterator++, position++)
@@ -77,12 +77,20 @@ void separator_parser(string &string_input)
         int ifWorkFind = string_input.find("&&",position);
             //looks for &&
         int ifFailFind = string_input.find("||",position);
-            //looks for ||
-        if(((NextFind < ifWorkFind && NextFind < ifFailFind) 
-            || (ifWorkFind == -1 && ifFailFind ==-1)
-            || (NextFind < ifWorkFind && ifFailFind ==-1)
-            || (NextFind < ifFailFind && ifWorkFind ==-1))
-            && (NextFind != -1))
+		//looks for ||
+	int appendFind = string_input.find(">>", position);
+		//looks for >>
+	int inputFind = string_input.find("<",position);
+		//looks for <
+	int outputFind = string_input.find(">",position);
+		//looks for >
+	int pipeFind = string_input.find("|",position);
+		//looks for |
+	if(NextFind != -1 
+	&& (NextFind < ifWorkFind || ifWorkFind == -1 ) 
+	&& (NextFind < ifFailFind || ifFailFind == -1) 
+	&& (NextFind < appendFind || appendFind == -1) 
+	&& (NextFind < inputFind || inputFind == -1))
             {
                 
                 //adds spaces around ; connector
@@ -91,32 +99,60 @@ void separator_parser(string &string_input)
                 my_iterator = string_input.begin() + string_input.find(";")+1;
                 position = string_input.find(";", position)+1;
             }
-        else if(((ifWorkFind < NextFind && ifWorkFind < ifFailFind) 
-            || (NextFind == -1 && ifFailFind ==-1)
-            || (ifWorkFind < NextFind && ifFailFind ==-1)
-            || (ifWorkFind < ifFailFind && NextFind ==-1))
-            && (ifWorkFind != -1))
+	else if(inputFind != -1 
+	    && (inputFind < ifWorkFind || ifWorkFind == -1 ) 
+	    && (inputFind < ifFailFind || ifFailFind == -1) 
+	    && (inputFind < appendFind || appendFind == -1) 
+	    && (inputFind < NextFind || NextFind == -1))
+            {
+                
+                //adds spaces around > connector
+                string_input.insert(inputFind, " ");
+                string_input.insert(string_input.find("<",position)+1," ");
+                my_iterator = string_input.begin() + string_input.find("<")+1;
+                position = string_input.find("<", position)+1;
+            }
+        else if(ifWorkFind != -1 
+	    && (ifWorkFind < NextFind || NextFind == -1 ) 
+	    && (ifWorkFind < ifFailFind || ifFailFind == -1) 
+	    && (ifWorkFind < appendFind || appendFind == -1) 
+	    && (ifWorkFind < inputFind || inputFind == -1))
         {
-            
+            cout << "welp" << endl;
             //adds spaces around && connector
             string_input.insert(ifWorkFind, " ");
             string_input.insert(string_input.find("&&",position)+2," ");
             my_iterator = string_input.begin() + string_input.find("&&")+2;
             position = string_input.find("&&", position)+2;
         }
-        else if(((ifFailFind < ifWorkFind && ifFailFind < NextFind) 
-            || (ifWorkFind == -1 && NextFind ==-1)
-            || (ifFailFind < ifWorkFind && NextFind ==-1)
-            || (ifFailFind < NextFind && ifWorkFind ==-1))
-            && (ifFailFind != -1))
+	else if(appendFind != -1 
+	    && (appendFind < NextFind || NextFind == -1 ) 
+	    && (appendFind < ifFailFind || ifFailFind == -1) 
+	    && (appendFind < ifWorkFind || ifWorkFind == -1) 
+	    && (appendFind < inputFind || inputFind == -1)	   )
         {
-        
+            cout << "obi" << endl;
+            //adds spaces around >>
+            string_input.insert(appendFind, " ");
+            string_input.insert(string_input.find(">>",position)+2," ");
+            my_iterator = string_input.begin() + string_input.find(">>")+2;
+            position = string_input.find(">>", position)+2;
+        }
+        else if(ifFailFind != -1 
+	    && (ifFailFind < NextFind || NextFind == -1 ) 
+	    && (ifFailFind < appendFind || appendFind == -1) 
+	    && (ifFailFind < ifWorkFind || ifWorkFind == -1) 
+	    && (ifFailFind < inputFind || inputFind == -1))
+        {
+        	cout << "heads" << endl;
             //adds spaces around || connector
             string_input.insert(ifFailFind, " ");
             string_input.insert(string_input.find("||",position)+2," ");
             my_iterator = string_input.begin() + string_input.find("||")+2;
             position = string_input.find("||", position)+2;
         }
+	counter++;
+	cout << counter << endl;
     }
     return;
 }
@@ -203,15 +239,171 @@ void run_command(char** command_list, bool &works)
     }
     return;
 }
+string redirection_parse(char** command)
+{
+	string current_redirect = command[0];
+	char** redirectiongroup = (char**)malloc(BUFSIZ);
+	
+	/*
+	bool containRedirection = false;
+	string append = ">>";
+	string output_redirect = ">";
+	string input_redirect = "<";
+	string pipe = "|";
+	string current_redirect;
+	size_t neverFound = -1;
+	while(toucanator != NULL && containRedirection == false)
+	{
+		string tempaStringa = command;
+		if(tempaStringa.find("<") != neverFound
+			|| tempaStringa.find("<<") != neverFound
+			|| tempaStringa.find("|") != neverFound)
+		{
+			containRedirection = true;
+		}
+		if(containRedirection == true)
+		{
 
+		}
+		else if(containRedirection == false)
+		{
+
+		}
+	}
+	if(containRedirection = false)
+	{
+		//return;
+	}
+	*/
+	return current_redirect;
+}
+void input_redirection()
+{
+	int pid;
+	int id;
+	int fd;
+	/*
+	pid = fork();
+	if(pid == -1)
+	{
+		perror("fork()");
+		_exit(1);
+	}
+	else if(pid == 0)
+	{
+		if((execvp(argv[0],argv) == -1)
+		{
+			perror("execvp()");
+		}
+		_exit(1);
+
+	}
+	else if(pid > 0)
+	{
+		if(wait(0) == -1)
+		{
+			perror("wait()");
+			_exit(1);
+		}
+	}
+	*/
+	return;
+	
+}
+
+void output_redirection()
+{
+
+	return;
+}
+void piping()
+{
+
+	return;
+}
+void redirection_separator(string& string_input)
+{
+/*
+	string::iterator my_iterator;
+	int position = 0;
+ 	for( my_iterator = string_input.begin();
+    	my_iterator < string_input.end();
+    	my_iterator++, position++)
+    	{
+ 		int inputFind = string_input.find("<", position);
+		int outputFind = string_input.find(">", position);
+		int appendFind = string_input.find("> >", position);
+		int pipeFind = string_input.find(" | ", position);
+        	if(((NextFind < ifWorkFind && NextFind < ifFailFind) 
+            || (ifWorkFind == -1 && ifFailFind ==-1)
+            || (NextFind < ifWorkFind && ifFailFind ==-1)
+            || (NextFind < ifFailFind && ifWorkFind ==-1))
+            && (NextFind != -1))
+            {
+                
+                //adds spaces around ; connector
+                string_input.insert(NextFind, " ");
+                string_input.insert(string_input.find(";",position)+1," ");
+                my_iterator = string_input.begin() + string_input.find(";")+1;
+                position = string_input.find(";", position)+1;
+            }
+        else if(((ifWorkFind < NextFind && ifWorkFind < ifFailFind) 
+            || (NextFind == -1 && ifFailFind ==-1)
+            || (ifWorkFind < NextFind && ifFailFind ==-1)
+            || (ifWorkFind < ifFailFind && NextFind ==-1))
+            && (ifWorkFind != -1))
+        {
+            
+            //adds spaces around && connector
+            string_input.insert(ifWorkFind, " ");
+            string_input.insert(string_input.find("&&",position)+2," ");
+            my_iterator = string_input.begin() + string_input.find("&&")+2;
+            position = string_input.find("&&", position)+2;
+        }
+        else if(((ifFailFind < ifWorkFind && ifFailFind < NextFind) 
+            || (ifWorkFind == -1 && NextFind ==-1)
+            || (ifFailFind < ifWorkFind && NextFind ==-1)
+            || (ifFailFind < NextFind && ifWorkFind ==-1))
+            && (ifFailFind != -1))
+        {
+        
+            //adds spaces around || connector
+            string_input.insert(ifFailFind, " ");
+            string_input.insert(string_input.find("||",position)+2," ");
+            my_iterator = string_input.begin() + string_input.find("||")+2;
+            position = string_input.find("||", position)+2;
+        }
+    }
+   */
+	return;
+}
 void run_command_with_connectors(char**& final_command,char* command_chara)
 {
         string nextGo = ";";
 	string workGo = "&&";
 	string failGo = "||";
-        char* tempa_token = strtok(command_chara," ");
+	cout << command_chara << endl;
+	/*
+	int findSemi = command_chara.strch(";");
+	int findAnd = command_chara.strch("&&");
+	int findOr  = command_chara.strch("||");
+	*/
+	char* tempa_token = strtok(command_chara," ");
+	/*
+	if(findSemi < findAnd && findSemi < findOr)
+	{
+		tempa_token = strtok(command_chara,";");
+	}
+	else if(findAnd < findSemi && findAnd < findOr)
+	{
+		tempa_token = strtok(command_chara,"&&");
+	}
+	else if(findOr < findSemi && findOr < findAnd)
+	{
+		tempa_token = strtok(command_chara,"||");
+	}
+	*/
         bool did_it_work = true;//checks if last command succeeded 
-	
 	//true if not supposed to run through the chain
 	bool chained_or = false;
 	bool chained_and = false;
@@ -219,6 +411,7 @@ void run_command_with_connectors(char**& final_command,char* command_chara)
 	string current_connect = ";";
 	while(tempa_token != NULL)
         {
+		//cout << tempa_token << endl;
 	//	cout << "current:" << current_connect << endl;
 		current_connect = connector_parse(final_command,tempa_token);
 		//parses input
@@ -240,6 +433,9 @@ void run_command_with_connectors(char**& final_command,char* command_chara)
 			}
 			else 
 			{
+				//cout << "ayy :";
+				//const char** false_command = final_command;
+				//cout << redirection_parse(final_command) << endl;
 				run_command(final_command,did_it_work);
 			}
 			chained_or = false;
@@ -265,6 +461,9 @@ void run_command_with_connectors(char**& final_command,char* command_chara)
 			else
 			{	    
 			//	cout << "c" << endl;
+				cout << "ayy :";
+				cout << redirection_parse(final_command) << endl;
+
 				run_command(final_command,did_it_work);
 				if(did_it_work == false)
 				{
@@ -291,7 +490,11 @@ void run_command_with_connectors(char**& final_command,char* command_chara)
 			}
 			else
 			{
-			//cout << "m" << endl;
+			//cout << "m" << endl;;
+				cout << "ayy :";
+				cout << redirection_parse(final_command)<< endl;
+
+
 			    run_command(final_command,did_it_work);
 			    if(did_it_work == true)
 			    {
@@ -327,11 +530,10 @@ int main(int argc, char**argv) {
 		char* command_char = new char[command_input.size()+1];
 		strcpy(command_char,command_input.c_str());
         	//converts strings into chars
-        char** finalist_command = (char**)malloc(BUFSIZ);
-
+       		 char** finalist_command = (char**)malloc(BUFSIZ);
 		run_command_with_connectors(finalist_command,command_char);
-        delete[] command_char;
-        free(finalist_command);
+        	delete[] command_char;
+        	free(finalist_command);
 		//attempts to run commands with connectors but is broken right now
         	//it runs everything, ignoring connectors
            
